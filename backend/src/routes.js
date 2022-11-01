0ab686212a5621419dcdc8b779653344c1c4db3d
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-10-31 21:06:16 -0300

    criação dos os padrões de dadastro dos users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import HelloController from './controllers/HelloController';
+import UsersConstroller from './controllers/UsersConstroller';
 
 const routes = new Router();
 
-routes.get('/hello', HelloController.index);
+routes.get('/users', UsersConstroller.index);
+routes.get('/users/:id', UsersConstroller.show);
+routes.post('/users', UsersConstroller.create);
+routes.put('/users/:id', UsersConstroller.update);
+routes.delete('/users/:id', UsersConstroller.destroy);
 
 export default routes;
