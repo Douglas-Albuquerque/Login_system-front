8d7722fd393176261b53eaf987693659f69867f3
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-01 12:03:20 -0300

    implementação de rota privada nas rotas

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,10 @@
-import { Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './Pages/HomePage';
-import LoginPage from './Pages/LoginPage';
+import AppRoutes from './Routes/index';
 
 function App() {
   return (
     <div className="App">
-      <header className='head'>
-        <nav>
-          <h1 className='textHead '>Oliveira Trade</h1>
-          <Link to="/">Login</Link>
-          <Link to="/home">Home</Link>
-        </nav>
-        <hr />
-      </header>
-      <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/home' element={<HomePage />} />
-      </Routes>
+      <AppRoutes />
     </div>
   );
 }
