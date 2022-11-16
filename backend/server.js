2e5de8b3bc17a3a0b359500c0d3220fcdbdd3049
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-16 20:48:23 -0300

    organizando brachs

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import app from './src/app';
-
-//app.listen(5000);
-app.listen(process.env.PORT || port, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("IT'S ALIVE");
-})
