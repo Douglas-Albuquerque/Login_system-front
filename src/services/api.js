9766dc587e52e676a616e079c3e124b9535276d4
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-21 19:27:20 -0300

    att'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://44.203.131.82:5000",
+  baseURL: "http://ec2-44-203-131-82.compute-1.amazonaws.com:5000",
   //baseURL: "http://localhost:5000",
 });
 
