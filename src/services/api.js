5a9904b73f745d69fd6c372b7e82ef3e565a50a1
douglas-albuquerque <douglasalbuquerq@gmail.com>
2022-11-21 20:28:06 -0300

    att

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://ec2-44-203-131-82.compute-1.amazonaws.com:5000",
-  //baseURL: "http://localhost:5000",
+  //baseURL: "http://ec2-44-203-131-82.compute-1.amazonaws.com:5000",
+  baseURL: "http://localhost:5000",
 });
 
 export const createSession = async (email, password) => {
